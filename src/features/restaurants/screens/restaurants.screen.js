dea43cfd6ef5052fa11bdda5a92835d35a52671b
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-05-09 12:20:25 +0100

    Implements flatlist component

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,27 +1,32 @@
 import React from "react";
 import { StatusBar } from "react-native";
 import Search from "../../../components/Search";
-import { Platform } from "react-native";
+import { Platform, FlatList } from "react-native";
 import RestaurantInfoCard from "../components/restaurant.info-card.component";
 import styled from "styled-components/native";
+import Spacer from "../components/spacer/spacer.component";
 
 const StyledSafeAreaView = styled.SafeAreaView`
   flex: 1;
   padding-top: ${Platform.OS === "ios" ? 0 : `${StatusBar.currentHeight}px`};
 `;
 
-const ListView = styled.View`
-  flex: 1;
-  padding: ${(props) => props.theme.space[3]};
-`;
-
 const RestaurantScreen = () => {
   return (
     <StyledSafeAreaView>
       <Search />
-      <ListView>
-        <RestaurantInfoCard />
-      </ListView>
+      <FlatList
+        data={[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }]}
+        renderItem={() => (
+          <>
+            <Spacer position="bottom" size="large">
+              <RestaurantInfoCard />
+            </Spacer>
+          </>
+        )}
+        keyExtractor={(item) => item.name}
+        contentContainerStyle={{ padding: 16 }}
+      />
     </StyledSafeAreaView>
   );
 };
