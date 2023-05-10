17c1a7f1355cc93539fadcbb2b106007f749a26a
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-05-10 11:14:00 +0100

    Transforms restaurant mock data using camelize package

---
+++ src/services/restaurants/restaurants.services.js
@@ -0,0 +1,31 @@
+import { mocks } from "./mock";
+import camelize from "camelize";
+
+export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
+  return new Promise((resolve, reject) => {
+    const mock = mocks[location];
+
+    if (!mock) {
+      reject("Not found");
+    }
+
+    resolve(mock);
+  });
+};
+
+export const restaurantTransform = ({ results }) => {
+  const mappedResult = results.map((restaurant) => {
+    return {
+      ...restaurant,
+      isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
+      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+    };
+  });
+
+  return camelize(mappedResult);
+};
+
+restaurantsRequest()
+  .then(restaurantTransform)
+  .then((transformedResponse) => console.log(transformedResponse))
+  .catch((error) => console.log("Error"));
