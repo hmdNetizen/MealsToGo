8505ea436eb96c9387bd69521c5deaa2e5f6cc97
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-05-15 17:01:46 +0100

    Implements lottie animation that displays the water melon in the account screen

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,9 +1,11 @@
 import React from "react";
+import LottieView from "lottie-react-native";
 import Spacer from "../../restaurants/components/spacer/spacer.component";
 import {
   AccountBackground,
   AccountContainer,
   AccountOverlay,
+  AnimationWrapper,
   AuthButton,
   Title,
 } from "../components/account.styles";
@@ -12,6 +14,15 @@ const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountOverlay />
+      <AnimationWrapper>
+        <LottieView
+          key="animation"
+          autoPlay
+          loop
+          resizeMode="cover"
+          source={require("../../../../assets/waitermelon.json")}
+        />
+      </AnimationWrapper>
       <Title>Meals To Go</Title>
       <AccountContainer>
         <AuthButton
