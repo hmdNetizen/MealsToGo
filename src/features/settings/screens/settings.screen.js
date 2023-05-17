214466559092d6c9d599888805a537c52ee93c42
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-05-17 11:50:32 +0100

    Adds avatar to the settings screen

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,24 +1,33 @@
 import React, { useContext } from "react";
-import { Text, Button } from "react-native";
-import { List } from "react-native-paper";
+import styled from "styled-components/native";
+import { List, Avatar } from "react-native-paper";
 
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 import { SafeArea } from "../../../components/utilities/safearea.component";
+
+const SettingsItem = styled(List.Item)`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const AvatarContainer = styled.View`
+  align-items: center;
+`;
 
 function SettingsScreen() {
   const { onLogout } = useContext(AuthenticationContext);
   return (
     <SafeArea>
+      <AvatarContainer>
+        <Avatar.Icon icon="human" size={180} />
+      </AvatarContainer>
       <List.Section>
-        <List.Item
+        <SettingsItem
           title="Favorites"
-          style={{ padding: 16 }}
           description="View your favorites"
           left={() => <List.Icon icon="heart" color="black" />}
         />
-        <List.Item
+        <SettingsItem
           title="Logout"
-          style={{ padding: 16 }}
           left={() => <List.Icon icon="logout" color="black" />}
           onPress={onLogout}
         />
